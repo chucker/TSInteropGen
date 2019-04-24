ee128dfd560a0517d316cc04acd766f1ee366119
chucker <chucker23n@gmail.com>
2019-04-24 21:51:49 +0200

    Add project files.

---
+++ TSInteropGen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TSInteropGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
